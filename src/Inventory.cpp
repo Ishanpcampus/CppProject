a8bea5bb8cc0ea22c76bbc1f4af599a497f39714
himanshu53929 <himanshuchandthakuri@gmail.com>
2025-07-10 16:58:26 +0545

    Inventory Added by Himanshu😁

---
+++ src/Inventory.cpp
@@ -0,0 +1,115 @@
+#include<iostream>
+#include "Inventory.h"
+#include<fstream>
+
+Inventory::Inventory()
+{
+
+}
+
+//Adds Items in the inventory
+void Inventory::addItem()
+{
+    int n;
+    std::cout<<"Enter the no. of items you want to add: ";
+    std::cin>>n;
+
+    for(int i=0; i<n; i++){
+        Item I;
+        std::cout<<"Enter the name of the "<<i+1<<" th item: "; 
+        std::cin>>I.itemName;
+
+        std::cout<<"Enter the quantity of the item: ";
+        std::cin>>I.quantity;
+
+        std::cout<<"Enter the price of the item: ";
+        std::cin>>I.price;
+
+        items.push_back(I);
+        
+    }
+
+    return;
+}
+
+//Updates price if needed
+void Inventory::updatePrice(const std::string nam, const int &p)
+{
+    for(Item &I: items){
+        if(nam == I.itemName){
+            I.price = p;
+        }
+    }
+
+    return;
+}
+
+//Updates inventory based on the quantity
+void Inventory::updateQuantity(const std::string nam, const int& q)
+{
+    for(Item &I: items){
+        if(nam == I.itemName){
+            I.quantity += q;
+        }
+    }
+
+    //I will also add some code here to check if the quantity is 0 and then remove it
+
+    return;
+}
+
+//Displays Item in the inventory
+void Inventory::viewInventory() const
+{
+    for(const Item& I: items){
+        std::cout<<"Name of item: "<<I.itemName<<std::endl<<"Quantity of item: "<<I.quantity<<
+            std::endl<<"Price of Item: "<<I.price<<std::endl;
+        
+        std::cout<<"\n";
+    }
+
+    return;
+}
+
+//Writes all items present in the vector to the file
+void Inventory::saveInventoryToFile(const std::string &filename) const
+{
+    std::ofstream outfile("inventory.txt");
+
+    for(const Item& I: items){
+        outfile<<I.itemName<<std::endl;
+        outfile<<I.quantity<<std::endl;
+        outfile<<I.price<<std::endl;
+    }
+
+    return;
+}
+
+//Reads all items from the inventory and assigns them to the vector
+void Inventory::loadInventoryFromFile()
+{
+    std::ifstream infile("inventory.txt");
+
+    while(!EOF){
+        Item I;
+        infile>>I.itemName;
+        infile>>I.quantity;
+        infile>>I.price;
+        items.push_back(I);
+    }
+
+    return;
+    
+}
+
+//Checks if the item is less than or equal to 15 and displays an alert message if it does
+void Inventory::checkReorderAlerts()
+{
+    for(const Item I: items){
+        if(I.quantity <= 15){
+            std::cout<<"The "<<I.itemName<<" is almost finished!!! You may reorder it."<<std::endl;
+        }
+    }
+
+    return;
+}
